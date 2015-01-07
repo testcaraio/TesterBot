0560e0f2b5805178abc4ad197ca275667cc7abd1
Big Boss <isabella_blood@live.com>
2015-01-07 01:09:01 -0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -274,7 +274,7 @@ basicBot.room.roulette.rouletteStatus = false;
 var ind = Math.floor(Math.random() * basicBot.room.roulette.participants.length);
 var winner = basicBot.room.roulette.participants[ind];
 basicBot.room.roulette.participants = [];
-var pos = Math.floor((Math.random() * API.getWaitList().length) 50 + 1);
+var pos = Math.floor((Math.random() * API.getWaitList().length) + 1);
 var user = basicBot.userUtilities.lookupUser(winner);
 var name = user.username;
 API.sendChat(subChat(basicBot.chat.winnerpicked, {name: name, position: pos}));
