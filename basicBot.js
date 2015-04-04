10cd1d141b6966dd3d95cf39e09f400d9cfcb01a
Big Boss <isabella_blood@live.com>
2015-04-04 17:32:08 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -276,7 +276,7 @@
             website: null,
             intervalMessages: [],
             messageInterval: 5,
-            songstats: true,
+            songstats: false,
             commandLiteral: "!",
             blacklists: {
                 NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
