5e417f651c8bf6cc8d179ec19dc6d0f2f3eb8fcb
Big Boss <isabella_blood@live.com>
2015-04-14 22:53:26 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -217,13 +217,13 @@
             usercommandsEnabled: true,
             lockskipPosition: 3,
             lockskipReasons: [
-                ["theme", "This song does not fit the room theme. "],
-                ["op", "This song is on the OP list. "],
-                ["history", "This song is in the history. "],
+                ["pqs", "Por que eu quis, palhaço. "],
+                ["op", "Esse video é repetido de mais. "],
+                ["history", "Esse video está no historico. "],
                 ["mix", "You played a mix, which is against the rules. "],
                 ["sound", "The song you played had bad sound quality or no sound. "],
-                ["nsfw", "The song you contained was NSFW (image or sound). "],
-                ["unavailable", "The song you played was not available for some users. "]
+                ["nsfw", "Esse video tem putaria/NSFW, é proibido porra (imagem). "],
+                ["bug", "Esse video está bugado. "]
             ],
             ball: [
             "Acho que sim cuzão.",
