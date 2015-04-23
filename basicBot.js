ce6c380504ab81a17fc6c4bee83b244e477616a4
Big Boss <isabella_blood@live.com>
2015-04-22 21:38:22 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1147,12 +1147,15 @@
                     }
                 }
                 basicBot.room.roomstats.chatmessages++;
-            },
+	  },
             spam: [
-                 'pula',
-            ]
+                'hueh', 'hu3', 'brbr', 'heu', 'brbr', 'kkkk', 'spoder', 'mafia', 'zuera', 'zueira',
+                'zueria', 'aehoo', 'aheu', 'alguem', 'algum', 'brazil', 'zoeira', 'fuckadmins', 'affff', 'vaisefoder', 'huenaarea',
+                'hitler', 'ashua', 'ahsu', 'ashau', 'lulz', 'huehue', 'hue', 'huehuehue', 'merda', 'pqp', 'puta', 'mulher', 'pula', 'retarda', 'caralho', 'filha', 'ppk',
+                'gringo', 'fuder', 'foder', 'hua', 'ahue', 'modafuka', 'modafoka', 'mudafuka', 'mudafoka', 'ooooooooooooooo', 'foda'
+            ],
             curses: [
-                'pulaa',
+                'nigger', 'faggot', 'nigga', 'niqqa', 'motherfucker', 'modafocka'
             ]
         },
         connectAPI: function () {
