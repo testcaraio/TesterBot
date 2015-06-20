525dd197f77e9a7bd59e671a86ae7a7f3afd92c8
PearlNegra <usernamejuntos@live.com>
2015-06-19 23:08:25 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1585,24 +1585,6 @@
                 }
             },
 
-            banCommand: {
-                command: ['ban'],
-                rank: 'bouncer',
-                type: 'startsWith',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        var msg = chat.message;
-                        if (msg.length === cmd.length) return API.sendChat(subChat(basicBot.chat.nouserspecified, {name: chat.un}));
-                        var name = msg.substr(cmd.length + 2);
-                        var user = basicBot.userUtilities.lookupUserName(name);
-                        if (typeof user === 'boolean') return API.sendChat(subChat(basicBot.chat.invaliduserspecified, {name: chat.un}));
-                        API.moderateBanUser(user.id, 1, API.BAN.DAY);
-                    }
-                }
-            },
-
             blacklistCommand: {
                 command: ['blacklist', 'bl'],
                 rank: 'bouncer',
